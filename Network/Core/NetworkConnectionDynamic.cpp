d49c18d15bf25ea1d7d1b1c5e56f58ca36dc7b0e
simonbe <benjaminsson@gmail.com>
2012-06-26 01:50:52 +0300

    Update master

--- Network/Core/NetworkConnectionDynamic.cpp
+++ Network/Core/NetworkConnectionDynamic.cpp
@@ -1,12 +1,1 @@
 #include "NetworkConnectionDynamic.h"
-
-
-ConnectionDynamic::ConnectionDynamic() :
-    tau_psc_(3.0),
-    tau_fac_(0.0),
-    tau_rec_(800.0),
-    U_(0.5),
-    x_(1.0),
-    y_(0.0),
-    u_(0.0)
-  { }
